b2f8ea1d74e373c29b392fc51ec7840d79941a86
iLtc <i@iltc.io>
2016-12-26 16:49:52 +0800

    Add Comment

--- media.py
+++ media.py
@@ -1,6 +1,14 @@
 import webbrowser
 
 class Movie:
+    """This is movie class
+
+    Attributes:
+        movie_title (str): The title of the movie
+        poster_image (str): The url address of the poster
+        trailer_youtube (str): The url of the trailer on youtube
+    """
+
     def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
